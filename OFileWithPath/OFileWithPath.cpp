b5c69b5a0800d1e62b6b600840062cfbf5b3c92c
ASUS <ASUS@AZIZDWIMUKTI>
2024-06-03 14:45:12 +0700

    input isi dari int main

--- OFileWithPath/OFileWithPath.cpp
+++ OFileWithPath/OFileWithPath.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menuklis
 
 }
