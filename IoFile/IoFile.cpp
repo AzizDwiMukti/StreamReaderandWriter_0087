20835635394e2128f43c753dbeee26adb3c5cdf5
ASUS <ASUS@AZIZDWIMUKTI>
2024-06-03 14:30:11 +0700

    menutup file

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -23,3 +23,12 @@ int main() {
 		//menuliskan dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 }
+	//selesai dalam menulis sekkarang tutup filenya
+	outfile.close();
+
+	// membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
