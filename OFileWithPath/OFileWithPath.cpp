c9cde23a33db09e2506f8d526dd83cc7c1bfa95f
ASUS <ASUS@AZIZDWIMUKTI>
2024-06-03 14:48:56 +0700

    membuat while trueinput data while inlimited loop untuk menulis

--- OFileWithPath/OFileWithPath.cpp
+++ OFileWithPath/OFileWithPath.cpp
@@ -18,5 +18,13 @@ int main() {
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
 	//unlimited loop untuk menuklis
-
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menuliskan dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
